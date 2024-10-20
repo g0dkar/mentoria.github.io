b49c3442eb6c9df842f4740447b28f7885ea69bc
Rafael Lins <rafael.araujo@swile.com.br>
2024-10-20 10:49:24 -0300

    Começando os trabalhos

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -6,27 +6,19 @@ const config: Config = {
   title: 'Mentoria Cloud',
   tagline: 'Aprendendo Programação do Zero',
   favicon: 'img/favicon.ico',
-
-  // Set the production url of your site here
   url: 'https://mentoria.cloud',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'g0dkar', // Usually your GitHub org/user name.
-  projectName: 'mentoria.github.io', // Usually your repo name.
+  organizationName: 'g0dkar',
+  projectName: 'mentoria.github.io',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'throw',
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'pt',
-    locales: ['pt'],
+    defaultLocale: 'pt-BR',
+    locales: ['pt-BR'],
   },
 
   presets: [
@@ -34,11 +26,8 @@ const config: Config = {
       'classic',
       {
         docs: {
+          routeBasePath: '/',
           sidebarPath: './sidebars.ts',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },/*
         blog: {
           showReadingTime: true,
@@ -66,9 +55,9 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: 'My Site',
+      title: 'Mentoria Cloud',
       logo: {
-        alt: 'My Site Logo',
+        alt: 'Logo da Mentoria Cloud',
         src: 'img/logo.svg',
       },
       items: [
@@ -76,25 +65,24 @@ const config: Config = {
           type: 'docSidebar',
           sidebarId: 'tutorialSidebar',
           position: 'left',
-          label: 'Tutorial',
-        },
-        {to: '/blog', label: 'Blog', position: 'left'},
+          label: 'Aprendendo a Programar',
+        },/*
         {
           href: 'https://github.com/facebook/docusaurus',
           label: 'GitHub',
           position: 'right',
-        },
+        },*/
       ],
     },
     footer: {
       style: 'dark',
       links: [
         {
-          title: 'Docs',
+          title: 'Aprendendo a Programar',
           items: [
             {
-              label: 'Tutorial',
-              to: '/docs/intro',
+              label: 'Introdução',
+              to: '/docs',
             },
           ],
         },
@@ -129,7 +117,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Ensinando desde 2024 - Construído com Docusaurus.`,
     },
     prism: {
       theme: prismThemes.github,
